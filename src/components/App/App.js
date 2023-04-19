aeb92084d3c9d9299da3fcdfff4ef68398cc0dcd
Kamajor <kamajorqa@gmail.com>
2023-04-19 23:47:13 +0300

    Подготовка стартового CRA проекта

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Hi there!
+        {5 + 5}
+      </p>
+    </div>
+  );
+}
+
+export default App;
