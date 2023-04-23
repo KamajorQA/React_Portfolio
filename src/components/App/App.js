9330b2a31fd7df703888f291dea12cf0dc5808a9
Kamajor <kamajorqa@gmail.com>
2023-04-23 18:06:03 +0300

    Переподключены шрифты, переменные вынесены в корневой index.css, навигация выделена в отдельный компонент

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,68 +1,16 @@
-import sun from '../../assets/img/icons/sun.svg';
-import moon from '../../assets/img/icons/moon.svg';
 import telegram from '../../assets/img/icons/telegram.svg';
 import gitHub from '../../assets/img/icons/gitHub.svg';
 import linkedIn from '../../assets/img/icons/linkedIn.svg';
 
 import './App.css';
+import Navigation from '../Navigaton/Navigation';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <nav className="nav">
-        <div className="container">
-          <div className="nav-row">
-            <a href="./index.html" className="logo">
-              <strong>Kamajor's</strong> portfolio
-            </a>
-
-            <button className="dark-mode-btn">
-              <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-              <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-            </button>
-
-            <ul className="nav-list">
-              <li className="nav-list__item">
-                <a
-                  href="./index.html"
-                  className="nav-list__link nav-list__link--active"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./projects.html" className="nav-list__link">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./contacts.html" className="nav-list__link">
-                  Contacts
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <header className="header">
-        <div className="header__wrapper">
-          <h1 className="header__title">
-            <strong>
-              Hi, my name is <em>Anton</em>
-            </strong>
-            <br />
-            I'm a frontend developer
-          </h1>
-          <div className="header__text">
-            <p>with passion for learning and creating.</p>
-          </div>
-          <a href="#!" className="btn">
-            Download CV
-          </a>
-        </div>
-      </header>
-
+      <Navigation />
+      <Header />
       <main className="section">
         <div className="container">
           <ul className="content-list">
