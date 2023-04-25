a1e88a8880c297c107eee88a6f0aff780cbbc832
Kamajor <kamajorqa@gmail.com>
2023-04-25 16:09:36 +0300

    1. Базовая кнопка доработана до универсальной для внешних и внутренних ресурсов. 2. На страницу конкретного проекта добавлена кнопка "назад". 3. В проект добавлена библиотека иконок FontAwesome.

--- src/components/ButtonOutline/ButtonOutline.jsx
+++ src/components/ButtonOutline/ButtonOutline.jsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './buttonOutline.css';
 
-function ButtonOutline({ icon, btnDesc, btnLink }) {
-  return (
+function ButtonOutline({ localIcon, faIcon, btnDesc, btnLink, inner }) {
+  return inner ? (
+    <Link to={btnLink} className="btn-outline">
+      {faIcon && <FontAwesomeIcon icon={faIcon} alt={btnDesc} />}
+      {btnDesc}
+    </Link>
+  ) : (
     <a href={btnLink} target="_blank" rel="noreferrer" className="btn-outline">
-      {icon && <img src={icon} alt={btnDesc} />}
+      {localIcon && <img src={localIcon} alt={btnDesc} />}
+      {faIcon && <FontAwesomeIcon icon={faIcon} alt={btnDesc} />}
       {btnDesc}
     </a>
   );
--- src/layouts/ProjectPage.jsx
+++ src/layouts/ProjectPage.jsx
@@ -1,6 +1,11 @@
 import { useParams } from 'react-router-dom';
-import projectsList from '../utilities/projectsList';
+import {
+  faAnglesLeft,
+  faUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons';
+import { faFilePowerpoint } from '@fortawesome/free-regular-svg-icons';
 import gitHubBlack from '../assets/img/icons/gitHub-black.svg';
+import projectsList from '../utilities/projectsList';
 import ButtonOutline from '../components/ButtonOutline/ButtonOutline';
 
 function ProjectPage() {
@@ -9,6 +14,14 @@ function ProjectPage() {
   return (
     <main className="section">
       <div className="container">
+        <section className="controls">
+          <ButtonOutline
+            faIcon={faAnglesLeft}
+            btnDesc={'назад'}
+            btnLink={'/projects'}
+            inner
+          />
+        </section>
         <div className="project-details">
           <h1 className="title-1">{currentProject.title}</h1>
           <img
@@ -23,16 +36,18 @@ function ProjectPage() {
             <p>Stack: {currentProject.stack}</p>
           </div>
           <ButtonOutline
+            faIcon={faUpRightFromSquare}
             btnDesc={'Visit project site'}
             btnLink={currentProject.deployLink}
           />
           <ButtonOutline
-            icon={gitHubBlack}
+            localIcon={gitHubBlack}
             btnDesc={'Code on GitHub'}
             btnLink={currentProject.gitHubLink}
           />
           {currentProject.presentation && (
             <ButtonOutline
+              faIcon={faFilePowerpoint}
               btnDesc={'View presentation'}
               btnLink={currentProject.presentation}
             />
