8c21fc5987608ff668607a6caad5a5b0d0515ddc
Kamajor <kamajorqa@gmail.com>
2023-10-09 23:20:16 +0300

    Обновление настроек CI/CD и данных о проектах.

--- README.md
+++ README.md
@@ -1,70 +1,25 @@
-# Getting Started with Create React App
+# Мое собственное портфолио, написанное на React
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+**_SPA-проект с информацией о применяемых мной технологиях и презентацией основных реализованных проектов_**
 
-## Available Scripts
+---
 
-In the project directory, you can run:
+### Использован шаблон c-r-a
 
-### `npm start`
+Для первичной инициализации проекта в его директории сперва нужно установить зависимости с помощью команды:
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+```sh
+npm install
+```
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Для локального запуска проекта нужно использовать команду
 
-### `npm test`
+```sh
+npm start
+```
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## В проекте настроен CI/CD через GitHub Actions.
 
-### `npm run build`
+![portfolio status badge](https://github.com/KamajorQA/React_Portfolio/actions/workflows/portfolio.yml/badge.svg)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+**_Деплой реализован через Netlify. Ссылка на продакшн стенд: [React_Portfolio](https://kamajor.netlify.app/)_**
--- src/utilities/projectsList.js
+++ src/utilities/projectsList.js
@@ -1,5 +1,11 @@
 import reactNews from '../assets/img/projects/ReactNewsPreview.png';
 import reactNewsBig from '../assets/img/projects/ReactNewsBig.png';
+import kittyPaws from '../assets/img/projects/KittyPawsPreview.png';
+import kittyPawsBig from '../assets/img/projects/KittyPawsBig.png';
+import fireBlog from '../assets/img/projects/FireBlogPreview.png';
+import fireBlogBig from '../assets/img/projects/FireBlogBig.png';
+import exquisiteGallery from '../assets/img/projects/ExquisiteGalleryPreview.png';
+import exquisiteGalleryBig from '../assets/img/projects/ExquisiteGalleryBig.png';
 import flappyBirds from '../assets/img/projects/FlappyBirdsPreview.png';
 import flappyBirdsBig from '../assets/img/projects/FlappyBirdsBig.png';
 import greedyDragons from '../assets/img/projects/GreedyDragonsPreview.png';
@@ -29,6 +35,80 @@ const projectsList = [
     slug: 'ReactNews',
     presentation:
       'https://drive.google.com/file/d/1tYtA18EhkXFyx7-d6n3kQ7KVHraIokYn/view',
+  },
+  {
+    title: 'Kitty Paws',
+    previewImg: kittyPaws,
+    bigImg: kittyPawsBig,
+    description: 'SPA table view database for cats on Firebase & TypeScript',
+    details: `React SPA-проект отображения в табличном виде списка котов из собственной NoSQL БД Firestore.
+    Реализованы все CRUD-операции и авторизация. 
+    Авторизация пользователей происходит согласно спецификации OAuth через провайдера Google посредством сервиса Firebase. 
+    Проект реализован в соответствии с поставленным ТЗ и требованиями, описанными в notion, включая
+    одновременную имплементацию легаси-подходов (классовые компоненты, библиотека PropTypes),
+     современных методов (TypeScript, функциональные компоненты), 
+     конкурирующих инструментов (React Context, Redux Toolkit, RTK Query, Firestore API). 
+    Интерфейс описан с помощью UI-библиотеки Ant Design и собственной CSS-стилизации.
+    Для отдельных элементов использованы библиотеки react-icons (svg-иконки) 
+    и  react-loader-spinner (спиннер).
+    Отображение страницы избранного реализовано в виде слайдера. 
+    Настроен CI/CD через GitHub Actions.
+    Деплой на Netlify. 
+    Бэкенд реализован самостоятельно через Firebase & Firestore. 
+    CRUD-операции реализованы через имплементацию связки Firestore API и RTK Query`,
+    stack:
+      'React, TypeScript, JavaScript, Firebase, Redux Toolkit, RTK Query, Ant Design, GitHub Actions',
+    gitHubLink: 'https://github.com/KamajorQA/Kitty_Paws',
+    deployLink: 'https://kitty-paws.netlify.app/',
+    slug: 'KittyPaws',
+  },
+  {
+    title: 'Fire Blog',
+    previewImg: fireBlog,
+    bigImg: fireBlogBig,
+    description: 'Blog website with Firebase backend and base testing',
+    details: `Блог на React с самостоятельно реализованным бэкендом на Firebase и базовым unit-тестированием. 
+    Настроен CI/CD через GitHub Actions.
+    Деплой на Netlify. 
+    Интерфейс описан с помощью UI-библиотеки Ant Design и собственной CSS-стилизации.
+    Реализована пагинация с возможностями выбора количества отображаемых карточек 
+    на странице и быстрым переходом к указанной странице. 
+    Авторизация пользователей происходит согласно спецификации OAuth 
+    через провайдера Google посредством сервиса Firebase. 
+    Для хранения данных использована собственная NoSQL БД Firestore.
+    CRUD-операции реализуются через Firestore API. 
+    Описаны базовые unit-тесты с jest-моками для компонентов, кастомных хуков и Redux-слайса`,
+    stack:
+      'React, JavaScript, Firebase, Firestore, Redux Toolkit, Jest, Ant Design',
+    gitHubLink: 'https://github.com/KamajorQA/Fire-Blog',
+    deployLink: 'https://fireblog-project.netlify.app/',
+    slug: 'FireBlog',
+  },
+  {
+    title: 'Exquisite Gallery',
+    previewImg: exquisiteGallery,
+    bigImg: exquisiteGalleryBig,
+    description: 'Gallery card list on React/TypeScript & Tailwind',
+    details: `Минималистичная галерея карточек товаров в виде SPA 
+    на React и TypeScript со стилизацией на Tailwind. 
+    Данные для проекта получаются из открытого API Fakestoreapi.com. 
+    Работа с данными на удаленном сервере (получение и добавление новой сущности) 
+    реализовано через нативный Fetch API. 
+    POST-запрос на добавление товара обрабатывается сервером с возвратом объекта добавляемого товара,
+    который сохраняется только в стейте списка продуктов.
+    В реальности моковая БД не изменяется, соответственно при повторном 
+    запросе списка товаров от сервера ранее добавленные продукты не отобразятся, 
+    что является нормальным поведением. 
+    В логике приложения выделены транспортный и логический слои. 
+    Транспортный слой вынесен в утилиту api.ts.
+    В нем реализуются только сами запросы к серверу. 
+    Работа с логикой вынесена в кастомные хуки.
+    Там же происходит обработка ответов сервера и отлов возможных ошибок.
+    Состояние и функции открытия/закрытия модального окна вынесены в контекст`,
+    stack: 'React, TypeScript, Tailwind, React Context, REST API',
+    gitHubLink: 'https://github.com/KamajorQA/React_Typescript_Gallery',
+    deployLink: 'https://exquisite-gallery.netlify.app/',
+    slug: 'ExquisiteGallery',
   },
   {
     title: 'FlappyBirds Game',
