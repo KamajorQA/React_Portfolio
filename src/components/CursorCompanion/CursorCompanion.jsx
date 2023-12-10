fc0493e282e38a865ccc24a26c5b089afbaa0c36
Kamajor <kamajorqa@gmail.com>
2023-12-10 22:32:32 +0300

    Реализован компонент анимированного курсора-сателлита.

---
+++ src/components/CursorCompanion/CursorCompanion.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from 'react';
+import {
+  motion,
+  AnimatePresence,
+  useSpring,
+  useMotionValue,
+} from 'framer-motion';
+import './cursorCompanion.css';
+
+function CursorCompanion({ hideCursor }) {
+  const cursorX = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
+
+  const springConfig = {
+    damping: 35,
+    stiffness: 700,
+    mass: 1,
+  };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      cursorX.set(e.clientX);
+      cursorY.set(e.clientY);
+    };
+
+    window.addEventListener('mousemove', moveCursor);
+
+    return () => {
+      window.removeEventListener('mousemove', moveCursor);
+    };
+  }, [cursorX, cursorY]);
+
+  return (
+    <motion.div
+      style={{
+        translateX: cursorXSpring,
+        translateY: cursorYSpring,
+      }}
+      className="cursorCompanion"
+    >
+      {!hideCursor == true ? (
+        <motion.div layoutId="cursor" className="ballPointer"></motion.div>
+      ) : null}
+    </motion.div>
+  );
+}
+
+export default CursorCompanion;
